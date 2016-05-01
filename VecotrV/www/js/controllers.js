b5d1edf1a922d7a64caa040fcf8e3c6b62c9be12
VipinAgarwal <vipinagarwal@live.com>
2016-05-01 02:42:57 -0700

    Code arranged with seperate controller file

--- VecotrV/www/js/controllers.js
+++ VecotrV/www/js/controllers.js
@@ -54,58 +54,7 @@ angular.module('app.controllers', [])
         
     })
 
-    .controller('selectConditionCtrl', function ($scope) {
-
-        var A = [0, .70, 1, 1.17, 1.34, 1.49, 1.63, 1.78, 1.93, 2.08];
-        var B = [0, .91, 1.21, 1.38, 1.55, 1.7, 1.84, 1.99, 2.14, 2.29];
-        var C = [0, .61, .91, 1.08, 1.25, 1.4, 1.54, 1.69, 1.84, 1.99];
-        var D = [0, .17, .47, .64, .81, .96, 1.1, 1.25, 1.4, 1.55];
-
-        $scope.result = {
-            L: { A: {}, B: {}, C: {}, D: {} },
-            LG: { A: {}, B: {}, C: {}, D: {} },
-            R: { A: {}, B: {}, C: {}, D: {} },
-            RG: { A: {}, B: {}, C: {}, D: {} }
-        }
-        var getValuefromDropDown = function (value) {
-            if (value == null || value == "-" || value == "") {
-                return 0;
-            }
-            else if (value == "A" || value == "B" || value == "C" || value == "D"){
-                return 1;
-            }
-            else return Number(value) + 1;
-        };
-
-        $scope.updateGraph = function (form) {
-            // $scope.$parent.patient.etestResults = {};
-            var eresult = { A: 0, B: 0, C: 0, D: 0 };
-            eresult.A = getValuefromDropDown(form.LA.$modelValue);
-            eresult.B = getValuefromDropDown(form.LB.$modelValue);
-            eresult.C = getValuefromDropDown(form.LC.$modelValue);
-            eresult.D = getValuefromDropDown(form.LD.$modelValue);
-            $scope.result.L = [{ x: 3, y: A[eresult.A] }, { x: 6, y: B[eresult.B] }, { x: 12, y: C[eresult.C] }, { x: 18, y: D[eresult.D] }];
-            eresult.A = getValuefromDropDown(form.LGA.$modelValue);
-            eresult.B = getValuefromDropDown(form.LGB.$modelValue);
-            eresult.C = getValuefromDropDown(form.LGC.$modelValue);
-            eresult.D = getValuefromDropDown(form.LGD.$modelValue);
-            $scope.result.LG = [{ x: 3, y: A[eresult.A] }, { x: 6, y: B[eresult.B] }, { x: 12, y: C[eresult.C] }, { x: 18, y: D[eresult.D] }];
-            eresult.A = getValuefromDropDown(form.RA.$modelValue);
-            eresult.B = getValuefromDropDown(form.RB.$modelValue);
-            eresult.C = getValuefromDropDown(form.RC.$modelValue);
-            eresult.D = getValuefromDropDown(form.RD.$modelValue);
-            $scope.result.R = [{ x: 3, y: A[eresult.A] }, { x: 6, y: B[eresult.B] }, { x: 12, y: C[eresult.C] }, { x: 18, y: D[eresult.D] }];
-            eresult.A = getValuefromDropDown(form.RGA.$modelValue);
-            eresult.B = getValuefromDropDown(form.RGB.$modelValue);
-            eresult.C = getValuefromDropDown(form.RGC.$modelValue);
-            eresult.D = getValuefromDropDown(form.RGD.$modelValue);
-            $scope.result.RG = [{ x: 3, y: A[eresult.A] }, { x: 6, y: B[eresult.B] }, { x: 12, y: C[eresult.C] }, { x: 18, y: D[eresult.D] }];
-           
-            //result.testdate = Date.now();
-            //$scope.$parent.patient.etestResults = result;
-        };
-
-    })
+  
 
     .controller('searchForPatientCtrl', function ($scope) {
 
@@ -137,28 +86,28 @@ angular.module('app.controllers', [])
         };
 
     })
-    .controller('eTestCtrl', function ($scope, $state) {
+    // .controller('eTestCtrl', function ($scope, $state) {
 
-        var getValuefromDropDown = function (value) {
-            if (value == null || value == "None") {
-                return 0;
-            }
-            else return value;
-        };
+    //     var getValuefromDropDown = function (value) {
+    //         if (value == null || value == "None") {
+    //             return 0;
+    //         }
+    //         else return value;
+    //     };
 
-        $scope.generateGraph = function (form) {
-            $scope.$parent.patient.etestResults = {};
-            var result = { A: {}, B: {}, C: {}, D: {} }
-            result.A = getValuefromDropDown(form.A.$modelValue);
-            result.B = getValuefromDropDown(form.B.$modelValue);
-            result.C = getValuefromDropDown(form.C.$modelValue);
-            result.D = getValuefromDropDown(form.D.$modelValue);
-            result.testdate = Date.now();
-            $scope.$parent.patient.etestResults = result;
+    //     $scope.generateGraph = function (form) {
+    //         $scope.$parent.patient.etestResults = {};
+    //         var result = { A: {}, B: {}, C: {}, D: {} }
+    //         result.A = getValuefromDropDown(form.A.$modelValue);
+    //         result.B = getValuefromDropDown(form.B.$modelValue);
+    //         result.C = getValuefromDropDown(form.C.$modelValue);
+    //         result.D = getValuefromDropDown(form.D.$modelValue);
+    //         result.testdate = Date.now();
+    //         $scope.$parent.patient.etestResults = result;
 
-            $state.go('egraph');
-        };
-    })
+    //         $state.go('egraph');
+    //     };
+    // })
     .controller('egraphCntrl', function ($scope, $cordovaPrinter) {
         var eresult = $scope.$parent.patient.etestResults;
         //test data.. 
